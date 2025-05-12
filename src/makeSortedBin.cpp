b916eed31517fbaec8ec19760c654df9f038eb7b
Guillermo Andrés Garda Cuevas <guillermoandresgardacuevas@gmail.com>
2025-05-11 21:11:22 -0400

    Eliminación de archivos obsoletos

--- src/makeSortedBin.cpp
+++ src/makeSortedBin.cpp
@@ -2,6 +2,16 @@
 #include <fstream>
 #include <string>
 
+/*
+    Crea un archivo binario de enteros ordenado en la carpeta bin.
+
+    Para usar compilar con: g++ makeSortedBin.cpp -o ex/makeSorted
+
+    Para crear un archivo: ./makeSorted <nombreArchivo> <tamaño en KB>
+
+    IMPORTANTE: El nombre del archivo no debe incluir .bin ni la ruta, solo el nombre.
+*/
+
 void generateSortedBinaryFile(const std::string& filename, size_t sizeKB, int start = 0, int step = 1) {
     size_t totalBytes = sizeKB * 1024;
     size_t numIntegers = totalBytes / sizeof(int);
